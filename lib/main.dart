1e3d5b2c95c6025463cff5dc4390eb452bce80a0
Aditiya <adityask345@gmail.com>
2023-09-09 23:18:41 +0600

    updated

--- lib/main.dart
+++ lib/main.dart
@@ -1,52 +1,111 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: ProfileScreen(),
+    return const MaterialApp(
+      title: 'Module 5 Assignment',
+      home: MyHomePage(),
     );
   }
 }
 
-class ProfileScreen extends StatelessWidget {
+class MyHomePage extends StatelessWidget {
+  const MyHomePage({super.key});
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("Profile"),
-        centerTitle: true,
+        backgroundColor: Colors.green,
+        elevation: 70.0,
+        toolbarHeight: 100.0,
+        title: const Center(
+          child: Text(
+            'Home',
+            style: TextStyle(
+              fontSize: 25.0,
+              fontWeight: FontWeight.bold,
+              color: Colors.white,
+            ),
+          ),
+        ),
+        leading: IconButton(
+          icon: const Icon(
+            Icons.add_business,
+            color: Colors.white,
+            size: 28.0,
+          ),
+          onPressed: () {
+            // Add your onPressed logic here
+          },
+        ),
+        actions: [
+          IconButton(
+            icon: const Icon(
+              Icons.search,
+              color: Colors.white,
+              size: 28.0,
+            ),
+            onPressed: () {
+              // Add your onPressed logic here
+            },
+          ),
+        ],
       ),
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            CircleAvatar(
-              backgroundColor: Colors.green,
-              radius: 30,
-              child: Icon(
-                Icons.person,
-                color: Colors.white,
-                size: 50,
-              ),
-            ),
-            SizedBox(height: 8), // Add some spacing
-            Text(
-              "Jhon Doe",
+          children: [
+            const Text(
+              'This is mod 5 Assignment',
               style: TextStyle(
-                color: Colors.green,
-                fontSize: 24,
+                fontSize: 19.0,
                 fontWeight: FontWeight.bold,
+                color: Colors.black,
               ),
             ),
-            Text(
-              "Flutter Batch 4",
-              style: TextStyle(
-                color: Colors.blue,
-                fontSize: 18,
+            const SizedBox(height: 10.0),
+            RichText(
+              text: const TextSpan(
+                style: TextStyle(
+                  fontSize: 21.0,
+
+                  color: Colors.red,
+                ),
+                children: [
+                  TextSpan(text: 'My '),
+                  TextSpan(
+                    text: 'phone ',
+                    style: TextStyle(
+                      fontSize: 15.0,
+                      color: Colors.lightBlueAccent,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  TextSpan(
+                    text: 'name ',
+                    style: TextStyle(
+                      fontSize: 19.0,
+                      color: Colors.purple,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  TextSpan(
+                    text:'Your phone name',
+                    style: TextStyle(
+                      fontSize: 23.0,
+                      color: Colors.orange,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ],
               ),
             ),
           ],
